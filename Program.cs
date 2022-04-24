3eaeece3571f8e6a7b5095e93ac26f42b1411731
Isabela Cruz <isabelacruz112@gmail.com>
2022-04-24 15:02:21 -0300

    alcançar peso ideal

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Entre com seu nome:");
 string nome = Console.ReadLine();
 
-Console.WriteLine ("Entre com sua altura. Ex: 1,65");
+Console.WriteLine ("Entre com sua altura em m. Ex: 1,65");
 float altura = Convert.ToSingle(Console.ReadLine());
 
 Console.WriteLine("Entre com seu peso em Kg. Ex: 67,7");
@@ -11,7 +11,7 @@ double imc = peso/ (altura * altura);
 
 Console.WriteLine ("{0} sua altura é {1}m e seu peso {2}kg", nome, altura, peso);
 
-Console.WriteLine("Seu IMC {0}", imc);
+Console.WriteLine("Seu IMC {0}", Math.Round(imc, 2));
 
 if(imc < 18.5)
 {
@@ -42,4 +42,18 @@ else
 {
     Console.WriteLine("Obesidade Grau III ou Mórbida.");
 }
-    
+
+
+if (imc < 18.5)
+{
+    double pesoIdeal = 18.5 * altura * altura; 
+    Console.WriteLine ("Para chegar ao peso ideal, você precisa ganhar: {0}kg", Math.Round(pesoIdeal - peso, 2));
+} 
+else if (imc > 24.9)
+{
+    double pesoIdeal = 24.9 * altura * altura; 
+    Console.WriteLine ("Para chegar ao peso ideal, você precisa perder: {0}kg", Math.Round(peso - pesoIdeal, 2));
+}
+
+
+
